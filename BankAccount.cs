1bebbe6ab807932ab1fba1449d7f2c869687cb20
kiruthigarajendran1903 <kiruthiga2807@gmail.com>
2023-07-26 15:26:06 +0530

    changes have been committed

---
+++ BankAccount.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+
+namespace ConAppAssignment16
+{
+    public class BankAccount
+    {
+        long accountNumber;
+        double balance;
+        public BankAccount(long accountNumber,string accountHolderName,double balance)
+        {
+            this.accountNumber = accountNumber;
+            this.AccountHolderName = accountHolderName;
+            this.balance = balance;
+        }
+      
+        public long AccountNumber 
+        { 
+            get
+            {
+                return accountNumber;
+            }
+        }
+        private string accountHolderName;
+       
+
+        public string AccountHolderName
+        {
+            get
+            {
+                return accountHolderName;
+            }
+            set
+            {
+                accountHolderName = value;
+            }
+        }
+
+        public void Deposit(double amount)
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine ("Deposit amount must be greater than zero.");
+            }
+
+            balance += amount;
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine("Withdrawal amount must be greater than zero.");
+            }
+
+            if (balance < amount)
+            {
+                Console.WriteLine ("Insufficient funds for withdrawal.");
+            }
+
+            balance -= amount;
+        }
+
+        public void PrintAccountDetails()
+        {
+            Console.WriteLine($"Account Number: {AccountNumber}");
+            Console.WriteLine($"Account Holder: {AccountHolderName}");
+            Console.WriteLine($"Balance: {balance:F2}");
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+
+namespace ConAppAssignment16
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+           
+                BankAccount bank = new BankAccount(38657856298769, "raj", 23000.56);
+                Console.WriteLine("Initial Account Details:");
+                bank.PrintAccountDetails();
+            //deposit
+            Console.WriteLine("Enter deposit amount");
+            double deposit=double.Parse(Console.ReadLine());
+            bank.Deposit(deposit);
+            Console.WriteLine("After deposit");
+            bank.PrintAccountDetails();
+            //withdraw
+            Console.WriteLine("Enter deposit amount");
+            double withdraw = double.Parse(Console.ReadLine());
+            bank.Withdraw(withdraw);
+            Console.WriteLine("After withdraw");
+            bank.PrintAccountDetails();
+
+            Console.WriteLine("Final Bank Statement");
+                bank.PrintAccountDetails();
+
+
+            BankAccount bank1 = new BankAccount(49923587697298, "vishunu", 78000.56);
+            Console.WriteLine("Initial Account Details:");
+            bank1.PrintAccountDetails();
+
+            //deposit
+            Console.WriteLine("Enter deposit amount");
+            double deposit1 = double.Parse(Console.ReadLine());
+            bank1.Deposit(deposit1);
+            Console.WriteLine("After deposit");
+            bank1.PrintAccountDetails();
+            //withdraw
+            Console.WriteLine("Enter deposit amount");
+            double withdraw1 = double.Parse(Console.ReadLine());
+            bank1.Withdraw(withdraw1);
+            Console.WriteLine("After withdraw");
+            bank1.PrintAccountDetails();
+            Console.WriteLine("Final Bank Statement");
+            bank1.PrintAccountDetails();
+
+            Console.ReadKey();
+
+        }
+    }
+}
